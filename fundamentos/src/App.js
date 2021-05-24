07303482db8909c38a938489b596306ca1835388
anajuliarossi <rossianajulia1@gmail.com>
2021-05-24 17:16:36 +0000

    App.js

--- fundamentos/src/App.js
+++ fundamentos/src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import Reac, {Component} from 'react'
+import OlaMundo from './componentes/basico/OlaMundo'
+class App extends Component{
+  render(){
+    return( 
+      <div> 
+        <h1>Aula 01 - Fundamentos 01</h1>
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        <OlaMundo/>
+
+      </div>
+    )
+  }
 }
 
 export default App;
